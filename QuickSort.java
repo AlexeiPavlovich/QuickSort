88b9797bdc15658cd651891ac58c8ee71e8d4efc
AlexeiPavlovich <75022771+AlexeiPavlovich@users.noreply.github.com>
2022-12-29 04:57:08 +0200

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -5,59 +5,33 @@ import java.util.Arrays;
 public class QuickSort {
 
 	public void sort(int[] array) {
-		System.out.println("sort");
-		System.out.println(Arrays.toString(array));
-		System.out.println();
-		quickSort(array, 0, array.length);
-		System.out.println("result");
-		System.out.println(Arrays.toString(array));
+		quickSort(array, 0, array.length - 1);
 	}
 
 	private void quickSort(int[] array, int left, int right) {
-		if (left < right) {
-			int partitionInd = partition(array, left, right);
-			quickSort(array, left, partitionInd);
-			quickSort(array, partitionInd + 1, right);
+		if (left >= right) {
+			return;
 		}
+		int partition = partition(array, left, right);
+		quickSort(array, left, partition - 1);
+		quickSort(array, partition + 1, right);
 
 	}
 
-	
-	
-	
 	private int partition(int[] array, int left, int right) {
-		System.out.println("partition");
-		printArray(array, left, right);
-		int pivot = array[left];
-		System.out.println("pivot: "+pivot);
-		int leftPos = left;
-		int rightPos = right;
-
-		while (leftPos < rightPos) {
-			do {
-				leftPos++;
-			} while (leftPos != right && array[leftPos] < pivot);
-
-			do {
-				rightPos--;
-			} while (rightPos != left && array[rightPos] > pivot);
-
-			if (leftPos < rightPos) {
-				swap(array, leftPos, rightPos);
+		int pivot = array[right];
+		int separateInd = left - 1;
+		for (int i = left; i < right; i++) {
+			if (array[i] < pivot) {
+				swap(array, i, ++separateInd);
 			}
-
 		}
-		swap(array, left, rightPos);
-		System.out.println("after partition");
-		printArray(array, left, right);
-		System.out.println();
-		return rightPos;
-
+		swap(array, right, ++separateInd);
+		return separateInd;
 	}
-	
 
 	private void printArray(int[] array, int left, int right) {
-		System.out.println(Arrays.toString(Arrays.copyOfRange(array, left, right)));
+		System.out.println(Arrays.toString(Arrays.copyOfRange(array, left, right + 1)));
 	}
 
 	private void swap(int[] array, int i, int j) {
